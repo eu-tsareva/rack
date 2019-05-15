87ac71af792dd5765dded4df5b094971f0c2277f
tsareva <e.tsareva@uit.space>
2019-05-15 19:34:48 +0300

    add main logic

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 class App
+  FORMATS = %w[year month day hour minute second].freeze
+
   def call(env)
     path = env['REQUEST_PATH']
-    query = URI.unescape(env['QUERY_STRING'])
+    query = CGI.unescape(env['QUERY_STRING'])
 
     response(path, query)
   end
@@ -9,18 +11,50 @@ class App
   private
 
   def response(path, query)
-    [status, headers, body(path, query)]
-  end
+    return page_not_found_response if path != '/time'
+    return unknown_query_response if query_attr(query) != 'format'
 
-  def status
-    200
+    formats = query_vals(query)
+    unknown = unknown_formats(formats)
+    if unknown.any?
+      unknown_formats_response(unknown)
+    else
+      formatted_time_reponse(formats)
+    end
   end
 
   def headers
     { 'Content-Type' => 'text/plain' }
   end
 
-  def body(path, query)
-    [path, query]
+  def query_attr(query)
+    query.split('=')[0]
   end
+
+  def query_vals(query)
+    query.split('=')[1].split(',')
+  end
+
+  def unknown_formats(formats)
+    formats - FORMATS
+  end
+
+  def page_not_found_response
+    [404, headers, ['Page not found']]
+  end
+
+  def unknown_query_response
+    [400, headers, ['Unknown query']]
+  end
+
+  def unknown_formats_response(formats)
+    [400, headers, ["Unknown time formats: [#{formats.join(', ')}]"]]
+  end
+
+  def formatted_time_reponse(formats)
+    time = Time.now
+    body = formats.map { |f| time.send(f) }.join('-')
+    [200, headers, [body]]
+  end
+
 end
