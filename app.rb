61155b6ef0b9918f50a3a40d836a6a01ac66b236
tsareva <e.tsareva@uit.space>
2019-05-18 00:19:52 +0300

    fix notes

--- app.rb
+++ app.rb
@@ -1,19 +1,37 @@
-require_relative 'response'
-require_relative 'time_app'
+require_relative 'time_query'
 
 class App
   def call(env)
-    path = env['REQUEST_PATH']
-    query = CGI.unescape(env['QUERY_STRING'])
+    request = Rack::Request.new(env)
 
-    route(path, query)
+    if request.path_info == '/time'
+      handle_valid_request(request.params['format'])
+    else
+      handle_invalid_request
+    end
   end
 
   private
 
-  def route(path, query)
-    return Response.new(status: 404).call if path != '/time'
+  def headers
+    { 'Content-Type' => 'text/plain' }
+  end
 
-    TimeApp.new(query).call
+  def handle_invalid_request
+    Rack::Response.new(['Page not found'], 404, headers)
+  end
+
+  def handle_valid_request(params)
+    time_query = TimeQuery.new(params)
+
+    if time_query.success?
+      Rack::Response.new([time_query.result], 200, headers)
+    else
+      Rack::Response.new(
+        ["Unknown formats [#{time_query.unknown_formats.join(', ')}]"],
+        400,
+        headers
+      )
+    end
   end
 end
